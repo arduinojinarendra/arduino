cad387848c2b0d3c001d117d0b78c7eb6db80012
narendrakesana <109544031+narendrakesana@users.noreply.github.com>
2022-07-18 22:42:48 +0530

    Add files via upload

---
+++ codes/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+int n=13;
+void setup() {
+  pinMode(n,OUTPUT);
+  // put your setup code here, to run once:
+}
+
+void loop() {
+
+  // put your main code here, to run repeatedly:
+}
